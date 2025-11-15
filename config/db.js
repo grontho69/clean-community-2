4d958dc2abf23cc4ca5206ec152e49d1691a4fab
grontho69 <mahathirm880@gmail.com>
2025-11-15 11:45:58 +0600

    first last

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 
-export default async function connectDB(){
-try{
-await mongoose.connect(process.env.MONGODB_URI);
-console.log('MongoDB Connected');
-}catch(err){
-console.error('DB Error', err);
+const connectDB = async () => {
+try {
+await mongoose.connect(process.env.MONGO_URI);
+console.log("MongoDB Connected");
+} catch (error) {
+console.error("MongoDB Error:", error);
 process.exit(1);
 }
-}
+};
+
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,30 +1,27 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import issueRoutes from "./routes/issues.js";
-import contributionRoutes from "./routes/contribution.js";
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
 
-dotenv.config();
 const app = express();
-
-
 app.use(cors());
 app.use(express.json());
 
 
-connectDB();
+app.use("/api/issues", require("./routes/issues"));
+app.use("/api/contribution", require("./routes/contribution"));
 
 
-app.get('/', (req,res)=>{
-res.send('Clean Community API Running');
+app.get("/", (req, res) => {
+res.send("Clean Community Server Running");
 });
 
 
-app.use('/api/issues', issueRoutes);
-app.use('/api/contributions', contributionRoutes);
-
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+connectDB();
+});
